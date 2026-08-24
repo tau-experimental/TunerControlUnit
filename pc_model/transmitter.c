c0eb60535d3eebb6702aa42ae75e2face7aa3a70
tau-experimental <invisible.kincajou@gmail.com>
2026-08-24 20:11:11 +0300

    перевод арифметики на fixed_point и табличные функции, теперь передатчик

--- pc_model/transmitter.c
+++ pc_model/transmitter.c
@@ -1,9 +1,9 @@
 #include "transmitter.h"
 #include "common.h"
+#include "fixed_point.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 int generate_fsk_wav(const unsigned char *payload, int payload_len, const double *freqs, const char *filename, int dirty) {
     int total_bytes, total_symbols, pause_samples, end_samples, signal_samples, total_samples;
@@ -11,15 +11,29 @@ int generate_fsk_wav(const unsigned char *payload, int payload_len, const double
     unsigned char *symbols;
     unsigned char crc = 0;
     int i, s_idx, t, out_idx;
-    double pause_sec, phase, double_rand;
+    double pause_sec, double_rand;
     FILE *f_out;
     WavHeader header;
+
+    /* Переменные для фиксированной точки DDS */
+    fixed_t fx_phase = 0;
+    fixed_t fx_phase_steps[4];
+    fixed_t fx_two_pi = TO_FX(2.0 * 3.141592653589793);
 
     if (payload_len < 0 || payload_len > MAX_PAYLOAD || !payload || !freqs || !filename) {
         return -1;
     }
 
-    total_bytes = 4 + payload_len + 1;
+    /*
+     * Предвычисляем фазовые шаги DDS для четырех частот 4-FSK.
+     * На МК эти коэффициенты будут вычислены заранее и лежать в памяти как константы fixed_t!
+     * phase_step = 2 * PI * f / FS
+     */
+    for (i = 0; i < 4; i++) {
+        fx_phase_steps[i] = TO_FX(2.0 * 3.141592653589793 * freqs[i] / (double)FS);
+    }
+
+    total_bytes = 4 + payload_len + 1; /* 2 Preamble + SOF + Len + Payload + CRC */
     total_symbols = total_bytes * 4;
 
     double_rand = (double)rand() / (double)RAND_MAX;
@@ -38,10 +52,12 @@ int generate_fsk_wav(const unsigned char *payload, int payload_len, const double
         return -1;
     }
 
+    /* Расчет контрольной суммы пакета */
     for (i = 0; i < payload_len; i++) {
         crc = update_crc8(crc, payload[i]);
     }
 
+    /* Сборка структуры кадра */
     byte_to_symbols(PREAMBLE_BYTE, &symbols[0]);
     byte_to_symbols(PREAMBLE_BYTE, &symbols[4]);
     byte_to_symbols(SOF_BYTE,       &symbols[8]);
@@ -54,20 +70,38 @@ int generate_fsk_wav(const unsigned char *payload, int payload_len, const double
     }
     byte_to_symbols(crc, &symbols[s_idx]);
 
-    phase = 0.0;
+    /*
+     * ЧЕСТНЫЙ ЦЕЛОЧИСЛЕННЫЙ ЦИФРОВОЙ СИНТЕЗ (DDS)
+     * Имитирует генерацию синусоиды через таймер ШИМ с 8-битным квантованием амплитуды.
+     */
     out_idx = pause_samples;
-
     for (i = 0; i < total_symbols; i++) {
-        double f = freqs[symbols[i]];
-        double phase_step = 2.0 * M_PI * f / (double)FS;
+        fixed_t step = fx_phase_steps[symbols[i]];
         for (t = 0; t < SYMBOL_LEN; t++) {
-            int val_8bit = (int)(sin(phase) * 127.0);
+            /* Вычисляем синус средствами целочисленной библиотеки fixed_t (Q16.16) */
+            fixed_t fx_sin_val = fx_sin(fx_phase);
+
+            /*
+             * Превращаем результат Q16.16 в честное 8-битное знаковое число от -128 до 127
+             * (Имитация ШИМ-ЦАП разрядности 8 бит).
+             * Так как fx_sin_val лежит в диапазоне [-FX_ONE .. FX_ONE], то умножение на 127
+             * через fx_mul вернет значение в шкале Q16.16, которое мы превращаем в обычное int.
+             */
+            fixed_t fx_scaled = fx_mul(fx_sin_val, INT_TO_FX(127));
+            int val_8bit = FX_TO_INT(fx_scaled);
+
+            /* Масштабируем до 16-битного PCM WAV файла (сдвиг влево на 8 бит) */
             buffer[out_idx++] = (short)(val_8bit << 8);
-            phase += phase_step;
-            if (phase > 2.0 * M_PI) phase -= 2.0 * M_PI;
+
+            /* Инкремент фазы аккумулятора DDS */
+            fx_phase += step;
+            if (fx_phase >= fx_two_pi) {
+                fx_phase -= fx_two_pi;
+            }
         }
     }
 
+    /* Наложение реалистичной грязи в кабель (Коричневый шум и импульсные щелчки) */
     if (dirty) {
         double brown_noise = 0.0;
         for (i = 0; i < total_samples; i++) {
@@ -85,6 +119,7 @@ int generate_fsk_wav(const unsigned char *payload, int payload_len, const double
         }
     }
 
+    /* Запись эмулированного кабеля в WAV файл */
     f_out = fopen(filename, "wb");
     if (!f_out) {
         free(buffer);
--- pc_model/transmitter.h
+++ pc_model/transmitter.h
@@ -1,6 +1,7 @@
 #ifndef TRANSMITTER_H
 #define TRANSMITTER_H
 
+/* Обновленный прототип: передатчик полностью оперирует на фиксированной точке */
 int generate_fsk_wav(const unsigned char *payload, int payload_len, const double *freqs, const char *filename, int dirty);
 
 #endif /* TRANSMITTER_H */
